16c8c9f7c9c207ce7470ae4cf00e9342364eee4d
Philipp Auersperg <phil@bluedynamics.com>
2012-07-27 12:06:49 +0200

    init

---
+++ src/agx/generator/sql/profiles.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from zope.interface import implements
+from agx.core.interfaces import IProfileLocation
+
+class ProfileLocation(object):
+
+    implements(IProfileLocation)
