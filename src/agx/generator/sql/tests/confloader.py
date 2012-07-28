0197a50864583b926ce9484dbc40cb43dbe445e9
Philipp Auersperg <phil@bluedynamics.com>
2012-07-28 11:52:53 +0200

    profile, sample model, tests

---
+++ src/agx/generator/sql/tests/confloader.py
@@ -0,0 +1,25 @@
+# Copyright BlueDynamics Alliance - http://bluedynamics.com
+# GNU General Public License Version 2
+
+from zope.interface import implements
+from zope.configuration.xmlconfig import XMLConfig
+from agx.core.interfaces import IConfLoader
+import agx.generator.uml
+import agx.generator.pyegg
+import agx.generator.zca
+import agx.generator.sql
+
+class ConfLoader(object):
+    
+    implements(IConfLoader)
+    
+    transforms = [
+        'xmi2uml',
+        'uml2fs',
+    ]
+    
+    def __call__(self):
+        XMLConfig('configure.zcml', agx.generator.uml)()
+        XMLConfig('configure.zcml', agx.generator.pyegg)()
+        XMLConfig('configure.zcml', agx.generator.zca)()
+        XMLConfig('configure.zcml', agx.generator.sql)()
